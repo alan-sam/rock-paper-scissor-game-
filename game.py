a509c410aa3a424ecb3f1cce86534ce32399de4c
alan-sam <66976535+alan-sam@users.noreply.github.com>
2022-09-25 16:29:27 -0500

    Create game.py

---
+++ game.py
@@ -0,0 +1,51 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+Choose = ["rock", "paper", "scissor"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissor or Type q to quit: ").lower()
+    if user_input == "q":
+
+        if user_wins > computer_wins:
+            print("User wins this game")
+        else:
+            print("Computer wins this game")
+        print("user wins", user_wins, "times")
+        print("computer wins", computer_wins, "times")
+        print("Goodbye! ")
+
+        quit()
+
+        break
+    if user_input not in Choose:
+        continue
+
+    randomNumber = random.randint(0,2)
+
+    computer_Choose = Choose[randomNumber]
+    print("computer picks", computer_Choose + '.')
+
+    if user_input == computer_Choose:
+        print("Its a draw ")
+        user_wins += 0
+        computer_wins += 0
+
+    elif user_input == "rock" and computer_Choose == "scissor":
+        print("You won")
+        user_wins += 1
+
+    elif user_input == "scissor" and computer_Choose == "paper":
+        print("You won")
+        user_wins += 1
+
+    elif user_input == "paper" and computer_Choose == "rock":
+        print("You won")
+        user_wins += 1
+
+    else:
+        print("computer won ")
+        computer_wins += 1
+
